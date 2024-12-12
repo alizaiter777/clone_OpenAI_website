44a42cc69299876dbd3f4691ba9850ca20fde5b2
alizaiter777 <alizaiter777@gmail.com>
2024-12-12 22:13:36 +0200

    generate display functions that on mouseover navbar element then go to new page

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,31 @@
-
+let isNavigating = false; 
 let hover;
+let hover2;
 document.addEventListener("DOMContentLoaded", loadPage);
 
 function display(){
+    if (!isNavigating) {
+        isNavigating = true;
     window.location.href = "research.html";
+    
+    
+}
+}
+function display2(){
+    if(!isNavigating){
+        isNavigating=true;
+    window.location.href = "index.html";
+    
+    }
 }
 
  
 function loadPage(){
     hover = document.getElementById("hover");
-    hover.addEventListener("mouseover", display);
+    hover2=document.getElementById("hover2")
+    hover.addEventListener("mouseover", display );
+    hover2.addEventListener("mouseover", display2);
+
    
 
 }
