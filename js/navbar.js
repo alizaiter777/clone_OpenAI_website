ba459285b9b9e95a55b940dc8a7f6a80a7f74c6e
alizaiter777 <alizaiter777@gmail.com>
2024-12-13 00:19:04 +0200

    change the mouseover way in js and use redirect page function

--- js/navbar.js
+++ js/navbar.js
@@ -1,31 +1,6 @@
-let isNavigating = false; 
-let hover;
-let hover2;
-document.addEventListener("DOMContentLoaded", loadPage);
 
-function display(){
-    if (!isNavigating) {
-        isNavigating = true;
-    window.location.href = "research.html";
-    
-    
-}
-}
-function display2(){
-    if(!isNavigating){
-        isNavigating=true;
-    window.location.href = "index.html";
-    
+        
+function redirectPage(url) {
+      window.location.href = url; 
     }
-}
-
- 
-function loadPage(){
-    hover = document.getElementById("hover");
-    hover2=document.getElementById("hover2")
-    hover.addEventListener("mouseover", display );
-    hover2.addEventListener("mouseover", display2);
-
-   
-
-}
+      
