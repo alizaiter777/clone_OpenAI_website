065e458c5874e71decb5af01d9558c163af0d993
alizaiter777 <alizaiter777@gmail.com>
2024-12-13 01:32:55 +0200

    make navbar responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,23 @@
 
-        
+
+document.addEventListener("DOMContentLoaded", loadPage);
+
 function redirectPage(url) {
-      window.location.href = url; 
+    window.location.href = url; 
+}
+
+function loadPage() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    console.log("menuToggle:", menuToggle);
+    console.log("navLinks:", navLinks);
+
+    if (menuToggle && navLinks) {
+        menuToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    } else {
+        console.error("Menu toggle or nav links element not found.");
     }
-      
+}
