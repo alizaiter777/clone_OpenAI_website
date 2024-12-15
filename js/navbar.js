f94ffa9c11abee85abe6cfefe4715bb3f4eb5c86
alizaiter777 <alizaiter777@gmail.com>
2024-12-15 22:49:46 +0200

    add products cards

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,19 @@
 
-
+let currentTranslate = 0;
 document.addEventListener("DOMContentLoaded", loadPage);
 
+    prevBtn.addEventListener('click', () => {
+      currentTranslate += cardWidth;
+      if (currentTranslate > 0) currentTranslate = 0;
+      cardRow.style.transform = `translateX(${currentTranslate}px)`;
+    });
+
+    nextBtn.addEventListener('click', () => {
+      currentTranslate -= cardWidth;
+      const maxTranslate = -(cardRow.scrollWidth - cardRow.clientWidth);
+      if (currentTranslate < maxTranslate) currentTranslate = maxTranslate;
+      cardRow.style.transform = `translateX(${currentTranslate}px)`;
+    });
 function redirectPage(url) {
     window.location.href = url; 
 }
@@ -9,6 +21,10 @@ function redirectPage(url) {
 function loadPage() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
+    const cardRow = document.getElementById('cardRow');
+    const prevBtn = document.getElementById('prev');
+    const nextBtn = document.getElementById('next');
+    const cardWidth = cardRow.querySelector('.card').offsetWidth;
 
     console.log("menuToggle:", menuToggle);
     console.log("navLinks:", navLinks);
@@ -23,3 +39,6 @@ function loadPage() {
 }
 
 
+
+
+  
